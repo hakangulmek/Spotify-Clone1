3bb440b763b530f8606c16a5686b810aa42aaa0d
Hakan Gülmek <gulmekhakan6@gmail.com>
2024-05-02 20:23:15 +0300

    Spotify_Clone

---
+++ Spotify_Clone/src/App.jsx
@@ -0,0 +1,10 @@
+import SideBar from "components/SideBar";
+import Content from "components/Content";
+import BottomBar from "components/BottomBar";
+import NavBar from "components/NavBar";
+function App() {
+  return;
+  <></>;
+}
+
+export default App;
